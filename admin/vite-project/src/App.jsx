741cd3acaf3bf1e2880fff6a3a5b3a61636b62f2
Prasad <p.khodade13@gmail.com>
2026-02-23 11:16:38 +0530

    Update backend URL in App component

--- admin/vite-project/src/App.jsx
+++ admin/vite-project/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
-  const url ="https://tomatoclone-backend-hsm0.onrender.com"
+  const url ="https://tomatoclone-back-end-fv7d.onrender.com"
   return (
     <div>
       <ToastContainer/>
